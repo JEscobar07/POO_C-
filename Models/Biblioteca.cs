471f67ad2eadf7fb23718a8333cecb85645be3e5
JEscobar07 <EscobarjonathanM@outlook.es>
2024-07-18 12:51:55 -0500

    feat:Adelanto de los metodos de la UML

--- Models/Biblioteca.cs
+++ Models/Biblioteca.cs
@@ -1,27 +1,52 @@
+using clase2.Models;
 namespace clase3.Models;
 public class Biblioteca{
     
     //Atributos
-    List<object> ListaDeLibros;
+    List<Libro> ListaDeLibros;
     //Constructor
-    public Biblioteca( List<object> _ListaDeLibros){
+    public Biblioteca( List<Libro> _ListaDeLibros){
         ListaDeLibros = _ListaDeLibros;
     }
 
     //Metodo
-    public void agregarLibro(){
+    public void agregarLibro(Libro librito){
+        var libroBuscado = ListaDeLibros.Where(item => item.Titulo.Equals(librito.Titulo,StringComparison.OrdinalIgnoreCase)).ToList();
+        if( libroBuscado == null){
+            mensaje("Se agrega el libro exitosamente");
+            ListaDeLibros.Add(librito);
+        }else{
+            mensaje("Lo sentimos pero ya hay un libro con el mismo titulo, ingresa otro diferente. :D");
+        }
+    }
+
+    public void eliminarLibro(Libro librito){
+        
+        var libroBuscado = ListaDeLibros.Where(item => item.Titulo.Equals(librito.Titulo,StringComparison.OrdinalIgnoreCase)).ToList();
+        if( libroBuscado != null){
+            mensaje("El libro ha sido eliminado exitosamente");
+            ListaDeLibros.Remove(librito);
+        }else{
+            mensaje("El libro que deseas eliminar no ha sido encontrado");
+        }
 
     }
 
-    public void eliminarLibro(){
+    public void buscarLibro(Libro librito){
 
-    }
-
-    public void buscarLibro(){
-
+        var libroBuscado = ListaDeLibros.Where(item => item.Titulo.Equals(librito.Titulo,StringComparison.OrdinalIgnoreCase));
+        if( libroBuscado != null){
+            mensaje($"El libro {librito.Titulo} se encuentra dentro de la biblioteca");
+        }else{
+            mensaje("El li");
+        }
     }
 
     public void DescuentoLibro(){
+        var libroBuscado = ListaDeLibros.
+    }
 
+    public void mensaje(string mensajito){
+        Console.WriteLine(mensajito);
     }
 }
--- Models/Libro.cs
+++ Models/Libro.cs
@@ -3,24 +3,23 @@ namespace clase2.Models;
 public class Libro : Publicacion{
     
     //Atributos
+    public Guid ISBN;
     public string Autor;
-    public string ISBN;
     public char Genero;
     public double Precio;
     
     //Constructor
-    public Libro(string _titulo,DateOnly _AñoDePublicacion, string _autor, string _isbn, char _genero, double _precio){
-        Id += 1;
+    public Libro(string _titulo,DateOnly _AñoDePublicacion, string _autor, char _genero, double _precio){
+        ISBN = Guid.NewGuid();
         Titulo = _titulo;
         AñoDePublicacion = _AñoDePublicacion;
         Autor = _autor;
-        ISBN = _isbn;
         Genero = _genero;
         Precio = _precio;
     }
 
     //Metodos
     public void DetalleDelLibro(){
-        Console.WriteLine($"Datos del libro:\nID: {Id}\nTitulo: {Titulo}\nAño de publicacion: {AñoDePublicacion}\nAutor: {Autor}\nISBN: {ISBN}\nGenero: {Genero}\nPrecio: {Precio}");
+        Console.WriteLine($"Datos del libro:\nID: {ISBN}\nTitulo: {Titulo}\nAño de publicacion: {AñoDePublicacion}\nAutor: {Autor}\nISBN: {ISBN}\nGenero: {Genero}\nPrecio: {Precio}");
     }
 }
