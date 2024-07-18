8b5c1f5b2e50bc119c51c3453af02fb98b9d3161
JEscobar07 <EscobarjonathanM@outlook.es>
2024-07-18 08:51:17 -0500

    feat: Se crea las 3 clases de acuerdo al UML

---
+++ Models/Biblioteca.cs
@@ -0,0 +1,27 @@
+namespace clase3.Models;
+public class Biblioteca{
+    
+    //Atributos
+    List<object> ListaDeLibros;
+    //Constructor
+    public Biblioteca( List<object> _ListaDeLibros){
+        ListaDeLibros = _ListaDeLibros;
+    }
+
+    //Metodos
+    public void agregarLibro(){
+        
+    }
+
+    public void eliminarLibro(){
+
+    }
+
+    public void buscarLibro(){
+
+    }
+
+    public void DescuentoLibro(){
+
+    }
+}
